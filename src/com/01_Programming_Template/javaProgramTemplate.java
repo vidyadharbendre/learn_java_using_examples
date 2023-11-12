a6d7391175e9c27e5cbaf60ddd6f3b133c3e6701
Vidyadhar Bendre <vidyadhar.bendre@gmail.com>
2023-11-12 18:40:52 +0530

    first program in java to print welcome message

---
+++ src/com/01_Programming_Template/javaProgramTemplate.java
@@ -0,0 +1,47 @@
+/*
+ * Program Name: MyJavaProgramTemplate.java
+ * Author: Vidyadhar Bendre
+ * Email: vidyadhar.bendre@gmail.com
+ * Date: November 23, 2023
+ * Description: This program demonstrates a basic Java programming template.
+ */
+
+// The 'package' statement is used to organize your classes into a package.
+// The package statement should reflect your project's directory structure.
+
+package com._01_Programming_Template;
+
+// Import statements are used to include classes from other packages.
+// In this case, we are importing the Scanner class from the java.util package.
+
+import java.util.Scanner;
+
+public class javaProgramTemplate {
+
+    // The 'public' keyword indicates that this class can be accessed from outside the package.
+    // The 'class' keyword is used to declare a class in Java.
+
+    public static void main(String[] args) {
+        // The 'public static void main(String[] args)' method is the entry point of the program.
+        // All Java programs must have a 'main' method where execution begins.
+
+        // This is a simple Java program that serves as a template.
+        // You can add your code here to achieve your desired functionality.
+        // Make sure to provide clear and meaningful comments to explain your code.
+
+        // The 'Scanner' class can be used to read input from the user.
+        // Uncomment the following lines if you want to use Scanner.
+
+        /*
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter something: ");
+        String userInput = scanner.nextLine();
+        System.out.println("You entered: " + userInput);
+        */
+
+        // Rest of the code...
+        System.out.println("Hello!, Welcome to Java Programming!");
+//        // Return to indicate successful execution.
+//        return;
+    }
+}
