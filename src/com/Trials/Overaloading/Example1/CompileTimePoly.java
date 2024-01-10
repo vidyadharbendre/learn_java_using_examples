b6d03888f3614ae0e4ca49b15f230d2cf868c8e5
Vidyadhar Bendre <vidyadharbendre@Vidyadhars-MacBook-Pro.local>
2024-01-10 12:58:19 +0530

    added poly

--- src/com/Trials/Overaloading/Example1/CompileTimePoly.java
+++ src/com/Trials/Overaloading/Example1/CompileTimePoly.java
@@ -1,24 +1,23 @@
-package com.Trials.LearnPolymorphism.Example_1;
+package com.Trials.Overaloading.Example1;
 
-public class polymorphism_CompileTime_learn_1 {
+public class CompileTimePoly {
     public static void main(String[] args) {
-
         Overloading overloadingRefObj1;
         overloadingRefObj1 = new Overloading();
         overloadingRefObj1.area();
 
-        com.Trials.LearnInheritance.Example_6.Overloading overloadingRefObj2;
-        overloadingRefObj2 = new com.Trials.LearnInheritance.Example_6.Overloading(2);
+        Overloading overloadingRefObj2;
+        overloadingRefObj2 = new Overloading(2);
         overloadingRefObj2.area(2);
 
 
-        com.Trials.LearnInheritance.Example_6.Overloading overloadingRefObj3;
-        overloadingRefObj3 = new com.Trials.LearnInheritance.Example_6.Overloading(3, 4);
+        Overloading overloadingRefObj3;
+        overloadingRefObj3 = new Overloading(3, 4);
         overloadingRefObj3.area(3);
         overloadingRefObj3.area(3, 4);
 
-        com.Trials.LearnInheritance.Example_6.Overloading overloadingRefObj4;
-        overloadingRefObj4 = new com.Trials.LearnInheritance.Example_6.Overloading(4, 5, 6);
+        Overloading overloadingRefObj4;
+        overloadingRefObj4 = new Overloading(4, 5, 6);
         overloadingRefObj4.area();
         overloadingRefObj4.area(4);
         overloadingRefObj4.area(4, 5);
