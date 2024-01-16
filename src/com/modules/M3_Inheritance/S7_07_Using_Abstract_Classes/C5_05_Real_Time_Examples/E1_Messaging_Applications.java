a9159edf2be3599d48f148d93d29554b2205035f
Vidyadhar Bendre <vidyadharbendre@Vidyadhars-MacBook-Pro.local>
2024-01-16 08:13:05 +0530

    added real time examples to demonstrate abstract class and methods

---
+++ src/com/modules/M3_Inheritance/S7_07_Using_Abstract_Classes/C5_05_Real_Time_Examples/E1_Messaging_Applications.java
@@ -0,0 +1,47 @@
+package com.modules.M3_Inheritance.S7_07_Using_Abstract_Classes.C5_05_Real_Time_Examples;
+
+public class E1_Messaging_Applications {
+    public static void main(String[] args) {
+        WhatsApp whatsapp = new WhatsApp();
+
+        // Example Usage:
+        whatsapp.sendMessage("Hello, How are you?", "Vidyadhar");
+        whatsapp.receiveMessage();
+        whatsapp.displayNotification();
+    }
+}
+
+abstract class MessagingApp {
+    abstract void sendMessage(String message, String recipient);
+    abstract void receiveMessage();
+    abstract void displayNotification();
+}
+
+class WhatsApp extends MessagingApp {
+    @Override
+    void sendMessage(String message, String recipient) {
+        // WhatsApp-specific implementation
+        System.out.println("WhatsApp Message Sent: \"" + message + "\" to " + recipient);
+    }
+
+    @Override
+    void receiveMessage() {
+        // WhatsApp-specific implementation
+        System.out.println("WhatsApp Message Received");
+    }
+
+    @Override
+    void displayNotification() {
+        // WhatsApp-specific implementation
+        System.out.println("WhatsApp Notification Displayed");
+    }
+}
+
+// The output of the above program is shown as below:
+/*
+
+WhatsApp Message Sent: "Hello, How are you?" to Vidyadhar
+WhatsApp Message Received
+WhatsApp Notification Displayed
+
+ */
---
+++ src/com/modules/M3_Inheritance/S7_07_Using_Abstract_Classes/C5_05_Real_Time_Examples/E2_Cooking_Receipes.java
@@ -0,0 +1,47 @@
+package com.modules.M3_Inheritance.S7_07_Using_Abstract_Classes.C5_05_Real_Time_Examples;
+
+public class E2_Cooking_Receipes {
+    public static void main(String[] args) {
+        PastaRecipe pastaRecipe = new PastaRecipe();
+
+        // Example Usage:
+        pastaRecipe.prepareIngredients();
+        pastaRecipe.cook();
+        pastaRecipe.serve();
+    }
+}
+
+abstract class Recipe {
+    abstract void prepareIngredients();
+    abstract void cook();
+    abstract void serve();
+}
+
+class PastaRecipe extends Recipe {
+    @Override
+    void prepareIngredients() {
+        // Pasta-specific implementation
+        System.out.println("Prepare Pasta Ingredients");
+    }
+
+    @Override
+    void cook() {
+        // Pasta-specific implementation
+        System.out.println("Cooking Pasta");
+    }
+
+    @Override
+    void serve() {
+        // Pasta-specific implementation
+        System.out.println("Serve Delicious Pasta");
+    }
+}
+
+// The output of the above program is shown as below
+/*
+
+Prepare Pasta Ingredients
+Cooking Pasta
+Serve Delicious Pasta
+
+ */
