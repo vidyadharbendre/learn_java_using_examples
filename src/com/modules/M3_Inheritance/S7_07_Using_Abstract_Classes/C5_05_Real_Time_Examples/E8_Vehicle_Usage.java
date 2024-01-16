793561b79de1a145cce28018f8f2ae414399a61e
Vidyadhar Bendre <vidyadharbendre@Vidyadhars-MacBook-Pro.local>
2024-01-16 22:37:02 +0530

    added Ecommerce and Vehicle examples to demonstrate abstract class with real-time scenarios

---
+++ src/com/modules/M3_Inheritance/S7_07_Using_Abstract_Classes/C5_05_Real_Time_Examples/E8_Vehicle_Usage.java
@@ -0,0 +1,62 @@
+package com.modules.M3_Inheritance.S7_07_Using_Abstract_Classes.C5_05_Real_Time_Examples;
+
+public class E8_Vehicle_Usage {
+    public static void main(String[] args) {
+        Car car = new Car();
+        Motorcycle motorcycle = new Motorcycle();
+
+        // Example Usage:
+        System.out.println("Car:");
+        car.start();
+        car.stop();
+
+        System.out.println("\nMotorcycle:");
+        motorcycle.start();
+        motorcycle.stop();
+    }
+}
+
+abstract class Vehicle {
+    protected String model;
+
+    abstract void start();
+
+    void stop() {
+        System.out.println("Vehicle stopped");
+    }
+}
+
+class Car extends Vehicle {
+    @Override
+    void start() {
+        // Car-specific implementation
+        System.out.println("Car started");
+    }
+}
+
+class Motorcycle extends Vehicle {
+    @Override
+    void start() {
+        // Motorcycle-specific implementation
+        System.out.println("Motorcycle started");
+    }
+
+    @Override
+    void stop() {
+        // Motorcycle-specific implementation
+        System.out.println("Motorcycle stopped");
+    }
+}
+
+// The output of the above program is shown as below:
+/*
+
+Car:
+Car started
+Vehicle stopped
+
+Motorcycle:
+Motorcycle started
+Motorcycle stopped
+
+ */
