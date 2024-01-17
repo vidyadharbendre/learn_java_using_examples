669c323a1b128ed71f21f5a9d9c18aa916902757
Vidyadhar Bendre <vidyadharbendre@Vidyadhars-MacBook-Pro.local>
2024-01-17 06:12:13 +0530

    modified the name of the derived class as Online Shopping Cart

--- src/com/modules/M3_Inheritance/S7_07_Using_Abstract_Classes/C5_05_Real_Time_Examples/E3_Online_Shopping_Cart.java
+++ src/com/modules/M3_Inheritance/S7_07_Using_Abstract_Classes/C5_05_Real_Time_Examples/E3_Online_Shopping_Cart.java
@@ -2,14 +2,14 @@ package com.modules.M3_Inheritance.S7_07_Using_Abstract_Classes.C5_05_Real_Time_
 
 public class E3_Online_Shopping_Cart {
     public static void main(String[] args) {
-        ShoppingCart shoppingCartRefObj;
-        shoppingCartRefObj = new ShoppingCart();
+        OnlineShoppingCart onlineShoppingCartRefObj;
+        onlineShoppingCartRefObj = new OnlineShoppingCart();
 
-        shoppingCartRefObj.addItem("Laptop", 120000);
-        shoppingCartRefObj.addItem("Headphone", 1000);
-        shoppingCartRefObj.addItem("mouse", 800);
-        shoppingCartRefObj.removeItem("Headphone");
-        shoppingCartRefObj.checkout();
+        onlineShoppingCartRefObj.addItem("Laptop", 120000);
+        onlineShoppingCartRefObj.addItem("Headphone", 1000);
+        onlineShoppingCartRefObj.addItem("mouse", 800);
+        onlineShoppingCartRefObj.removeItem("Headphone");
+        onlineShoppingCartRefObj.checkout();
 
     }
 
@@ -23,7 +23,7 @@ abstract class Cart{
 
 }
 
-class ShoppingCart extends Cart{
+class OnlineShoppingCart extends Cart{
     @Override
     void addItem(String itemName, double price) {
         System.out.println("Added " + itemName + " to the cart Price "+ price);
@@ -47,7 +47,7 @@ class ShoppingCart extends Cart{
 Added Laptop to the cart Price 120000.0
 Added Headphone to the cart Price 1000.0
 Added mouse to the cart Price 800.0
-Removed Headphone from the Cart
+Removed Headphone from the Cart 
 Checked Out. Thank you for Shopping
 
  */
