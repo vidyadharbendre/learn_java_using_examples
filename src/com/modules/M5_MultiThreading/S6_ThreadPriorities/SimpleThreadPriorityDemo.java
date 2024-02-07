269c3de0ea0979483f1527b18bfd9646a1fd23f4
Vidyadhar Bendre <vidyadharbendre@Vidyadhars-MacBook-Pro.local>
2024-02-07 14:10:26 +0530

    setting priority for a thread to print Hi first and later Hello

--- src/com/modules/M5_MultiThreading/S6_ThreadPriorities/SimpleThreadPriorityDemo.java
+++ src/com/modules/M5_MultiThreading/S6_ThreadPriorities/SimpleThreadPriorityDemo.java
@@ -1,23 +1,22 @@
 package com.modules.M5_MultiThreading.S6_ThreadPriorities;
 
-
-class MessageHi extends Thread{
+class MessageHi extends Thread {
     @Override
-    public void run(){
+    public void run() {
         for (int i = 0; i < 100; i++) {
             System.out.println("Hi");
-        }
-        try {
-            Thread.sleep(100);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
         }
     }
 }
 
-class MessageHello extends Thread{
+class MessageHello extends Thread {
     @Override
-    public void run(){
+    public void run() {
         for (int i = 0; i < 100; i++) {
             System.out.println("Hello");
             try {
@@ -29,35 +28,16 @@ class MessageHello extends Thread{
     }
 }
 
-
 public class SimpleThreadPriorityDemo {
     public static void main(String[] args) {
-        MessageHi messageHiRefObj;
-        messageHiRefObj = new MessageHi();
+        MessageHi messageHiRefObj = new MessageHi();
+        MessageHello messageHelloRefObj = new MessageHello();
 
+        // Set priority of Hi thread to a higher value
+        messageHiRefObj.setPriority(Thread.MAX_PRIORITY);
 
-
+        // Start the threads
         messageHiRefObj.start();
-
-        //print the current priority
-//        System.out.println("Priority of the Hi Thread : "+messageHiRefObj.getPriority());
-
-        try {
-            Thread.sleep(10);
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-        MessageHello messageHelloRefObj;
-        messageHelloRefObj = new MessageHello();
         messageHelloRefObj.start();
-
-//        messageHelloRefObj.setPriority(10);
-//        System.out.println("Priority of the Hello Thread : "+messageHelloRefObj.getPriority());
-
-//        messageHiRefObj.setPriority(Thread.MIN_PRIORITY);
-        System.out.println("Priority of the Hi Thread : "+messageHiRefObj.getPriority());
-
-
-
     }
 }
