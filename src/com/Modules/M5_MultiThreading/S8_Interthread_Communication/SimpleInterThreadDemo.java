660270b49cbf3d6a1f7efea6cf058575c16be835
Vidyadhar Bendre <vidyadharbendre@Vidyadhars-MacBook-Pro.local>
2024-02-14 19:36:41 +0530

    modified simpleinterthreaddemo

--- src/com/Modules/M5_MultiThreading/S8_Interthread_Communication/SimpleInterThreadDemo.java
+++ src/com/Modules/M5_MultiThreading/S8_Interthread_Communication/SimpleInterThreadDemo.java
@@ -3,10 +3,8 @@ package com.Modules.M5_MultiThreading.S8_Interthread_Communication;
 class Q {
     private int num;
     private boolean valueSet = false;
-    // Flag to indicate whether a value has been set
 
     public synchronized void getNum() {
-        // Wait until the value is set by the producer
         while (!valueSet) {
             try {
                 wait();
@@ -16,13 +14,10 @@ class Q {
         }
         System.out.println("Consumer got num value : " + num);
         valueSet = false;
-        // Reset the flag after consuming the value
         notify();
-        // Notify the producer that the consumer has consumed the value
     }
 
     public synchronized void setNum(int num) {
-        // Wait until the value is consumed by the consumer
         while (valueSet) {
             try {
                 wait();
@@ -32,9 +27,8 @@ class Q {
         }
         System.out.println("Producer set num value : " + num);
         this.num = num;
-        valueSet = true; // Set the flag to indicate that a value has been set
+        valueSet = true;
         notify();
-        // Notify the consumer that a new value is available
     }
 }
 
@@ -45,16 +39,14 @@ class Producer implements Runnable {
     public Producer(Q qRefObj, int count) {
         this.qRefObj = qRefObj;
         this.count = count;
-
-        Thread t1_RefObj;
-        t1_RefObj = new Thread(this, "Producer");
-        t1_RefObj.start();
     }
 
     @Override
     public void run() {
         for (int i = 0; i < count; i++) {
-            qRefObj.setNum(i);
+            synchronized (qRefObj) {
+                qRefObj.setNum(i);
+            }
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
@@ -71,16 +63,14 @@ class Consumer implements Runnable {
     public Consumer(Q qRefObj, int count) {
         this.qRefObj = qRefObj;
         this.count = count;
-
-        Thread t2_RefObj;
-        t2_RefObj = new Thread(this, "Consumer");
-        t2_RefObj.start();
     }
 
     @Override
     public void run() {
         for (int i = 0; i < count; i++) {
-            qRefObj.getNum();
+            synchronized (qRefObj) {
+                qRefObj.getNum();
+            }
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
@@ -94,12 +84,19 @@ public class SimpleInterThreadDemo {
     public static void main(String[] args) {
         Q q;
         q = new Q();
-        int numIterations = 10; // Number of iterations
+        int numIterations = 5; // Number of iterations
 
-        new Producer(q, numIterations);
-        new Consumer(q, numIterations);
+        Thread producerThread;
+        producerThread = new Thread(new Producer(q, numIterations));
+
+        Thread consumerThread;
+        consumerThread = new Thread(new Consumer(q, numIterations));
+
+        producerThread.start();
+        consumerThread.start();
     }
 }
+
 // The output of the above program is shown as below
 /*
 Producer set num value : 0
@@ -112,14 +109,4 @@ Producer set num value : 3
 Consumer got num value : 3
 Producer set num value : 4
 Consumer got num value : 4
-Producer set num value : 5
-Consumer got num value : 5
-Producer set num value : 6
-Consumer got num value : 6
-Producer set num value : 7
-Consumer got num value : 7
-Producer set num value : 8
-Consumer got num value : 8
-Producer set num value : 9
-Consumer got num value : 9
  */
